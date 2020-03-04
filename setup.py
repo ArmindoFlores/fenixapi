3e736f43cee85e36dda76570490b130153abdf80
Francisco Rodrigues <francisco.rodrigues0908@gmail.com>
2020-03-04 10:52:17 +0000

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="fenixapi-ArmindoFlores", # Replace with your own username
+    name="fenixapi",
     version="0.0.1",
     author="Francisco Rodrigues",
     author_email="francisco.rodrigues0908@gmail.com",
